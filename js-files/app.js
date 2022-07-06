2bc84b8fd9bc534a7f9c80bf3437461ebd8f4d41
ovsanna <ovsanna@ovsanna>
2022-07-06 15:58:03 +0400

    added figma to html images

--- js-files/app.js
+++ js-files/app.js
@@ -6,18 +6,16 @@ function pageTransitions() {
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', () => {
             let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
+            currentBtn[0].classList.remove("active-btn");
             let activeBtn = document.querySelectorAll('.control')[i];
             activeBtn.className += ' active-btn';
             // Add active class to section
             let sectionId = activeBtn.dataset.id;
             let currentSection = document.querySelectorAll('.active');
-            currentSection[0].className = currentSection[0].className.replace('active','');
+            currentSection[0].classList.remove("active");
             let activeSection = document.getElementById(sectionId);
             activeSection.className += ' active';
         })
     }
 }
-if(allSections.length != 0) {
-    pageTransitions();
-}
+pageTransitions();
