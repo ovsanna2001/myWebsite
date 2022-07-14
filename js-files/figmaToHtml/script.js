49ee8803f4de09115bac896eb4357b551bb13212
ovsanna <ovsanna@ovsanna>
2022-07-14 15:29:37 +0400

    added changes in desktop part

--- js-files/figmaToHtml/script.js
+++ js-files/figmaToHtml/script.js
@@ -4,6 +4,21 @@ const swiper = new Swiper('.swiper1', {
   loop: true,
   slidesPerView: 5,
   spaceBetween: 60,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 1024px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1351px
+    1351: {
+      slidesPerView: 5,
+      spaceBetween: 20
+    }
+  }
 });
 $('.accordion-header').click(function() {
     $(this).parent().toggleClass('show');
